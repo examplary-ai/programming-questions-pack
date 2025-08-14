fe608217ef4fbf4c12e2c82260bc0e9f296b5209
Thomas Schoffelen <thomas@schof.co>
2025-08-14 16:00:22 +0100

    chore: add assertions documentation

--- javascript/component-settings-area.tsx
+++ javascript/component-settings-area.tsx
@@ -22,7 +22,40 @@ const SettingsAreaComponent: FrontendQuestionSettingsAreaComponent = ({
           <summary className="text-sm text-zinc-600 cursor-pointer">
             {t("assertions-description")}
           </summary>
-          TODO
+          <div className="my-2 rounded bg-zinc-100 p-4 text-sm">
+            Assertions use{" "}
+            <a
+              href="https://jestjs.io/docs/expect"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold underline"
+            >
+              Jest's expect
+            </a>{" "}
+            function to assert the expected output of the code. The
+            documentation linked provides more information on how to use it and
+            the available matchers.
+            <br />
+            <br />
+            You can also build a test suite by using the <code>
+              describe
+            </code>{" "}
+            and <code>it</code> functions, which allows you to group tests and
+            show specific feedback to the student:
+            <pre className="rounded bg-white mt-1.5 p-2 text-xs">
+              <code>
+                {`it('result exists', () => {
+  expect(result).toBeDefined();
+});
+it('result is an array', () => {
+  expect(Array.isArray(result)).toEqual(true);
+});
+it('result contains 1,2,3', () => {
+  expect(result).toEqual([1, 2, 3]);
+});`}
+              </code>
+            </pre>
+          </div>
         </details>
         <CodeEditor
           value={settings["assertions"] || ""}
